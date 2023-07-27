329161cda2f7164c5c293a6507bbc32bc59635ea
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2023-07-26 20:19:24 -0500

    Create an .env file and set our environment variables in settings file from Backend

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -11,8 +11,13 @@ https://docs.djangoproject.com/en/4.2/ref/settings/
 """
 
 from pathlib import Path
+from corsheaders.defaults import default_headers
 import environ
 import os
+
+env = environ.Env()
+
+environ.Env.read_env()
 
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
@@ -23,10 +28,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/4.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = 'django-insecure-zm2w7+d^sy55e1r&q2-l$ecu&f^$a-)ew_oev@92^512x!e3t0'
+SECRET_KEY = env('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = env('DEBUG')
 
 ALLOWED_HOSTS = []
 
