8a6d742a10b8af84d5708e4ee1a393c9d1d74969
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2023-07-27 14:00:45 -0500

    Configure Static files (CSS, JavaScript, Images) in settings file

--- backend/core/settings.py
+++ backend/core/settings.py
@@ -151,7 +151,14 @@ USE_TZ = True
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.2/howto/static-files/
 
-STATIC_URL = 'static/'
+STATIC_URL = '/static/'
+MEDIA_URL = '/media/'
+
+MEDIA_ROOT = os.path.join(BASE_DIR, 'media')
+
+STATICFILES_DIRS = [
+    os.path.join(BASE_DIR, 'static')
+]
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.2/ref/settings/#default-auto-field
