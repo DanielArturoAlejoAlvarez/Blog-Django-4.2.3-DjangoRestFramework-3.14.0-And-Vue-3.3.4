09ae11a54671b2e2c5069b86e08a69be102bf814
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2023-07-27 20:12:21 -0500

    Creating Post model and user_directory_path to upload images in Blog

--- backend/blog/models.py
+++ backend/blog/models.py
@@ -1,3 +1,32 @@
 from django.db import models
+from django.db.models.query import QuerySet
 
-# Create your models here.
+from django.utils import timezone
+from django.contrib.auth.models import User
+
+
+def user_directory_path(instance,filename):
+    return 'blog/{0}/{1}'.format(instance.title, filename)
+
+
+class Post(models.Model):
+
+    
+    options = (
+        ('draft','Draft'),
+        ('published','Published'),
+    )
+
+    title=models.CharField(max_length=250)
+    thumbnail=models.ImageField(upload_to=user_directory_path, blank=True, null=True)
+    excerpt=models.TextField(null=True)
+    content=models.TextField()
+    slug=models.SlugField(max_length=250, unique_for_date='published', null=False, unique=True)
+    published=models.DateTimeField(default=timezone.now)
+
+    author=models.ForeignKey(User, on_delete=models.CASCADE, related_name='post_user')
+    status=models.CharField(max_length=10, choices=options, default='draft')
+
+    
+    
+    
