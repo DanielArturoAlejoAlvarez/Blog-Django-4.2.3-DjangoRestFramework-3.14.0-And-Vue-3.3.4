64b66472474988a5843a99be99cc1543d0ebc609
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2023-08-02 15:18:54 -0500

    Creating DetailPostView class from Blog views api

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -9,6 +9,13 @@ class BlogListView(APIView):
     def get(self,request,*args,**kwargs):
         posts=Post.postobjects.all()[0:4]
 
-        serializer=PostSerializer(posts, many=True)
+        serializer=PostSerializer(posts, many=True)#FIXME: using many posts reference
 
         return Response(serializer.data)
+    
+class DetailPostView(APIView):
+    def get(self,request,post_slug,*args,**kwargs):
+        post=get_object_or_404(Post, slug=post_slug)#FIXME: func get_object_or_404 return Obj or HttpException 404 
+        serializer=PostSerializer(post)  
+
+        return Response(serializer.data)     
