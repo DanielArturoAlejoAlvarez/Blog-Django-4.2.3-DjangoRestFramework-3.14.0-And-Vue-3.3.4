e818fb8190c1818a8a7aeec28b9bd5b9abc39c6a
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2023-08-02 18:49:45 -0500

    Create frontend with Vue3

--- backend/blog/urls.py
+++ backend/blog/urls.py
@@ -4,7 +4,6 @@ from .views import BlogListView, DetailPostView
 
 app_name='blog'
 
-
 urlpatterns = [
     path('posts/', BlogListView.as_view()),
     path('posts/<post_slug>', DetailPostView.as_view()),
