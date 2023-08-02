e67c3b35384c06f3cdf7b0299a693992e22e5f4c
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2023-08-02 11:03:17 -0500

    Creating BlogListView class from Blog views only filtered post published status

--- backend/blog/views.py
+++ backend/blog/views.py
@@ -1,3 +1,14 @@
-from django.shortcuts import render
+from django.shortcuts import render,get_object_or_404
 
-# Create your views here.
+from rest_framework.views import APIView
+from rest_framework.response import Response
+from .models import Post
+from .serializers import PostSerializer
+
+class BlogListView(APIView):
+    def get(self,request,*args,**kwargs):
+        posts=Post.postobjects.all()[0:4]
+
+        serializer=PostSerializer(posts, many=True)
+
+        return Response(serializer.data)
