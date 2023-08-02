37dda39b15370bee5539044d42f6d712df0858a6
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2023-08-02 15:38:44 -0500

    Configure routes to views to Rest Api Blog app

---
+++ backend/blog/urls.py
@@ -0,0 +1,11 @@
+from django.urls import path
+
+from .views import BlogListView, DetailPostView
+
+app_name='blog'
+
+
+urlpatterns = [
+    path('posts/', BlogListView.as_view()),
+    path('posts/<post_slug>', DetailPostView.as_view()),
+]
--- backend/core/urls.py
+++ backend/core/urls.py
@@ -21,6 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
+    path('blog/', include('blog.urls',namespace='blog')),
     path('api-auth/', include('rest_framework.urls')),
 ]
 
