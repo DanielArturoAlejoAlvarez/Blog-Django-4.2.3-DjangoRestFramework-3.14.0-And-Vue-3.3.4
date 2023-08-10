696151f67b7e2a13e5db57ee0d26aee26a2bc802
DanielArturoAlejoAlvarez <yourmediasoft@gmail.com>
2023-08-09 21:24:35 -0500

    Refactoring code frontend

--- frontend/src/views/BlogView.vue
+++ frontend/src/views/BlogView.vue
@@ -1,5 +1,7 @@
 <template>
-  <div class="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8">
+  <div
+    class="relative bg-gray-50 pt-16 pb-20 px-4 sm:px-6 lg:pt-24 lg:pb-28 lg:px-8"
+  >
     <div class="absolute inset-0">
       <div class="bg-white h-1/3 sm:h-2/3" />
     </div>
@@ -9,51 +11,53 @@
           <HelloWorld msg="Discover with me the most incredible posts" />
         </div>
       </div>
-      <div class="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none">
-        <div v-for="posts in APIData" :key="posts.id" class="flex flex-col rounded-lg shadow-lg overflow-hidden">
-        
-            <router-link :to="{ name: 'BlogPost', params: { id: posts.id, title: posts.title, thumbnail: posts.thumbnail, excerpt: posts.excerpt, content: posts.content, slug: posts.slug } }">
-                <img :src="base + posts.thumbnail" />
-                <div class="p-4">
-                  <p class="text-3xl text-red-600 font-bold mb-3">{{posts.title}}</p>
-                  <p class="text-gray-700 font-medium">{{posts.content}}</p>
-                </div>
-            </router-link>
-        
+      <div
+        class="mt-12 max-w-lg mx-auto grid gap-5 lg:grid-cols-3 lg:max-w-none"
+      >
+        <div
+          v-for="posts in APIData"
+          :key="posts.id"
+          class="flex flex-col rounded-lg shadow-lg overflow-hidden"
+        >
+          <router-link :to="{ name: 'BlogPost', params: { slug: posts.slug } }">
+            <img :src="base + posts.thumbnail" />
+            <div class="p-4">
+              <p class="text-3xl text-red-600 font-bold mb-3">
+                {{ posts.title }}
+              </p>
+              <p class="text-gray-700 font-medium">{{ posts.content }}</p>
+            </div>
+          </router-link>
         </div>
       </div>
     </div>
-
   </div>
 </template>
 
-
 <script>
-
-import HelloWorld from '@/components/HelloWorld.vue'
-import {getAPI} from '../assets/js/api.js'
+import HelloWorld from "@/components/HelloWorld.vue";
+import { getAPI } from "../assets/js/api.js";
 
 export default {
-    
-    name: 'BlogView',
-    components: {
-      HelloWorld
-    },
-    data(){
-      return {
-        APIData: [],
-        base: getAPI.defaults.baseURL,
-      }
-    },
-    created(){
-      getAPI.get('/blog/posts/')
-        .then(res=>{
-          console.log('Post API has received data')
-          this.APIData = res.data
-        })
-        .catch(err=>{
-          console.log(err)
-        })
-    }
-}
+  name: "BlogView",
+  components: {
+    HelloWorld,
+  },
+  data() {
+    return {
+      APIData: [],
+      base: getAPI.defaults.baseURL,
+    };
+  },
+  created() {
+    getAPI
+      .get("/blog/posts/")
+      .then((res) => {
+        this.APIData = res.data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
+};
 </script>
--- frontend/src/views/PostDetailView.vue
+++ frontend/src/views/PostDetailView.vue
@@ -1,53 +1,58 @@
 <template>
-    <div class="relative py-16  overflow-hidden">
-
+  <div class="relative py-16 overflow-hidden">
     <div class="relative px-4 sm:px-6 lg:px-8">
-      <div class="max-w-2xl text-lg mx-auto shadow-md p-2">
-        <div class="flex bg-red-700 text-white text-xl"><img class="mr-2" width="28" src="../assets//img/time-and-calendar.png"/>{{this.post.published}}</div>
-        <img :src="base + this.post.thumbnail" />
-        
-        <h1>
-          <span class="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl">{{this.post.title}}</span>
-        </h1>
-        <p class="mt-8 text-2xl font-bold text-gray-700 leading-8">{{this.post.excerpt}}</p>
-      
-        <div class="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">
-          {{this.post.content}}
+      <div class="max-w-2xl text-lg mx-auto shadow-lg p-2">
+        <div class="flex bg-red-700 text-white text-xl">
+          <img
+            class="mr-2"
+            width="28"
+            src="../assets//img/time-and-calendar.png"
+          />{{ this.post.published }}
         </div>
-      
+        <img :src="base + this.post.thumbnail" />
+
+        <h1>
+          <span
+            class="mt-2 block text-3xl text-center leading-8 font-extrabold tracking-tight text-gray-900 sm:text-4xl"
+            >{{ this.post.title }}</span
+          >
+        </h1>
+        <p class="mt-8 text-2xl font-bold text-gray-700 leading-8">
+          {{ this.post.excerpt }}
+        </p>
+
+        <div class="mt-6 prose prose-indigo prose-lg text-gray-500 mx-auto">
+          {{ this.post.content }}
+        </div>
       </div>
-      
     </div>
   </div>
 </template>
 
 <script>
-
-import HelloWorld from '@/components/HelloWorld.vue'
-import {getAPI} from '../assets/js/api.js'
+import HelloWorld from "@/components/HelloWorld.vue";
+import { getAPI } from "../assets/js/api.js";
 
 export default {
-    
-    name: 'PostDetailView',
-    components: {
-      HelloWorld
-    },
-    data(){
-      return {
-        post: {},
-        base: getAPI.defaults.baseURL
-      }
-    },
-    created(){
-      
-      getAPI.get(`/blog/posts/${this.$route.params.slug}`)
-        .then(res=>{
-          console.log('Post API has received data')
-          this.post = res.data
-        })
-        .catch(err=>{
-          console.log(err)
-        })
-    }
-}
+  name: "PostDetailView",
+  components: {
+    HelloWorld,
+  },
+  data() {
+    return {
+      post: {},
+      base: getAPI.defaults.baseURL,
+    };
+  },
+  created() {
+    getAPI
+      .get(`/blog/posts/${this.$route.params.slug}`)
+      .then((res) => {
+        this.post = res.data;
+      })
+      .catch((err) => {
+        console.log(err);
+      });
+  },
+};
 </script>
